473e9406eb330d804d9efb995de101cd66dc8f54
Goyfiver <miguelkenardo@hotmail.com>
2021-08-30 18:33:07 -0600

    Changes

--- main.js
+++ main.js
@@ -1,3 +1,49 @@
+const showMenu = (toggleId, navId)=>{
+    const toggle = document.getElementById(toggleId)
+    nav = document.getElementById(navId)
+
+    if(toggle && nav){
+        toggle.addEventListener('click', ()=>{
+            nav.classList.toggle('show')
+        })
+    }
+}
+
+
+showMenu('nav-toggle','nav-menu')
+
+/*Remove Menu*/
+const navLink =  document.querySelectorAll('.nav__link'),
+    navMenu = document.getElementById('nav-menu')
+
+function linkAction(){
+    navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/* Scroll Sections Active Link*/
+const Sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    Sections.forEach(current=>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.add('active')
+        }else{
+            document.querySelector('.nav__menu a[href*=' + sectionId + ']').classList.remove('active')
+        }
+    })
+}
+
+
 let skylineBack = document.getElementById('skylineBack');
         let skylineMid = document.getElementById('skylineMid');
         let text = document.getElementById('text');
